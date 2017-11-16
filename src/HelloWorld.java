26f80a51fbb1f533eb5b94c9f99d2e94cb245bb6
sschutzman <$t735815>
2017-11-16 16:56:56 -0600

    What is this I don't even

---
+++ src/HelloWorld.java
@@ -0,0 +1,33 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int myFavoriteNumber = 11;
+        String myString = "That was my lucky number" + 's';
+        double myNumber = 3.14159;
+        int x = 4;
+        x += 5;
+
+        int x = 3;
+        int y = 4;
+//        new vauile for old var name = old value fro old var name *x
+
+        y =* x;
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+
+
+        System.out.println(x++);
+        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        System.out.println(myNumber);
+        System.out.println(myString);
+        System.out.println(myFavoriteNumber);
+        System.out.print("Hello, World!");
+    }
+}
