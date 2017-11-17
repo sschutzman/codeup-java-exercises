37a626e477b5e0a2bdba8d94cde9056caffe5e88
sschutzman <$t735815>
2017-11-17 09:27:42 -0600

    Fixed broken things

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,23 +3,32 @@ public class HelloWorld {
         int myFavoriteNumber = 11;
         String myString = "That was my lucky number" + 's';
         double myNumber = 3.14159;
-        int x = 4;
+
+        int x = 5;
+
+        System.out.println(x++);
+        System.out.println(x);
+
+        int y = 5;
+        // ++y is pre-incrementing
+        System.out.println(++y);
+        System.out.println(y);
+
+        x = 4;
+        // newValueForVariable = oldValueForVariable + 5;
         x += 5;
 
-        int x = 3;
-        int y = 4;
-//        new vauile for old var name = old value fro old var name *x
+        x = 3;
+        y = 4;
+        //        new value for old var name = old value fro old var name *x
+        y *= x;
 
-        y =* x;
-
-        int x = 10;
-        int y = 2;
+        x = 10;
+        y = 2;
         x /= y;
         y -= x;
 
 
-        System.out.println(x++);
-        System.out.println(x);
 
 //        int x = 5;
 //        System.out.println(++x);
